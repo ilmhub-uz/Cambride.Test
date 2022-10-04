194875887db46707a305b10be373add6f56f79f9
Leverow <info.enjoymedia@mail.ru>
2022-10-04 17:32:29 +0500

    FileRepository added

--- Cambridge.Test.Data/Repository/FileRepository.cs
+++ Cambridge.Test.Data/Repository/FileRepository.cs
@@ -1,16 +1,25 @@
 using Cambridge.Test.Data.Abstractions.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cambridge.Test.Data.Repository;
 
 public class FileRepository : IFileRepository
 {
-    public ValueTask<Abstractions.Entity.File> GetFileOrDefaultAsync(Guid? id, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly AppDbContext _context;
 
-    public ValueTask<Abstractions.Entity.File> InsertAsync(Abstractions.Entity.File file, CancellationToken cancellationToken = default)
+    public FileRepository(AppDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    public async ValueTask<Abstractions.Entity.File?> GetFileOrDefaultAsync(Guid? id, CancellationToken cancellationToken = default)
+        => await _context.Set<Abstractions.Entity.File>().FirstOrDefaultAsync(f => f.Id == id);
+
+    public async ValueTask<Abstractions.Entity.File> InsertAsync(Abstractions.Entity.File file, CancellationToken cancellationToken = default)
+    {
+        var entry = await _context.Set<Abstractions.Entity.File>().AddAsync(file);
+
+        await _context.SaveChangesAsync();
+
+        return entry.Entity;
     }
 }
